7354a74312bf7e03df5920f6fe909a439e5e634b
BhavyaSri M <vjcbangalore@gmail.com>
2026-07-16 14:45:12 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,21 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
+import LeadProfileHistory from "./LeadProfileHistory";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/lead-history/:id"
+          element={
+            <ProtectedRoute>
+              <LeadProfileHistory />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/*"
           element={
