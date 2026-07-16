5b7f2a74c747b0aef00418bece578b9342cd0185
BhavyaSri M <vjcbangalore@gmail.com>
2026-07-16 14:49:54 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import LeadProfileHistory from "./LeadProfileHistory";
+import LeadProfileHistory from "./pages/LeadProfileHistory";
 
 function App() {
   return (
